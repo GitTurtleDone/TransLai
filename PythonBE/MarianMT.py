009316433054496a1673944c4fd45d3452d4890a
GitTurtleDone <giang2110@gmail.com>
2025-05-10 09:29:56 +1200

    Installed coqui-tts, worked well. About to instal viXTTS-demo, which will remove TTS

--- PythonBE/MarianMT.py
+++ PythonBE/MarianMT.py
@@ -5,7 +5,7 @@ tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name)
 
 # Define the path to your .txt file
-file_path = "./Experiments/whisper.cpp/samples/RogerPenrose.wav.txt"
+file_path = "./PythonBE/RogerPenroseEn.txt"
 
 # Read the contents of the file
 with open(file_path, 'r', encoding='utf-8') as file:
@@ -17,3 +17,23 @@ gen = model.generate(**batch)
 translation = tokenizer.decode(gen[0], skip_special_tokens=True)
 
 print(translation)
+
+
+model_name = 'Helsinki-NLP/opus-mt-vi-en'  # Example: Vietnamese to English
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
+# Define the path to your .txt file
+file_path = "./PythonBE/EmDiHocSomVi.txt"
+
+# Read the contents of the file
+with open(file_path, 'r', encoding='utf-8') as file:
+    text = file.read()
+
+#text = "Einstein is a theoretical physicist, who created the theory of general relativity."
+batch = tokenizer([text], return_tensors="pt")
+gen = model.generate(**batch)
+translation = tokenizer.decode(gen[0], skip_special_tokens=True)
+
+print(translation)
+
--- PythonBE/coqui-tts.py
+++ PythonBE/coqui-tts.py
@@ -1,9 +1,52 @@
+# import torch
+# from TTS.api import TTS
+# from TTS.tts.configs.xtts_config import XttsConfig
+# from TTS.utils.synthesizer import Synthesizer
+# import os
+
+# # Allow the config class to be safely loaded
+# torch.serialization.add_safe_globals([XttsConfig])
+
+# # Get device
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# # List available 🐸TTS models
+# print(TTS().list_models())
+
+# # Init TTS
+# #tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+
+# # Define the path to your .txt file
+# text_file_path = "PythonBE/RogerPenroseEn.txt"
+# text_file_path_vi = "PythonBE/EmDiHocSomVi.txt"
+# tts_share_dir = "~/.local/share/tts/tts_models--multilingual--multi-dataset--xtts_v2"
+
+# # Read the contents of the file
+# with open(text_file_path_vi, 'r', encoding='utf-8') as file:
+#     text = file.read()
+
+# synthesizer = Synthesizer(
+#     tts_checkpoint="~/.local/share/tts/tts_models--multilingual--multi-dataset--xtts_v2",
+#     tts_config_path="~/.local/share/tts/tts_models--multilingual--multi-dataset--xtts_v2/config.json",
+#     #tts_speakers_file="path/to/speakers.json",   # optional
+#     #tts_languages_file="path/to/languages.json", # optional
+#     #vocoder_checkpoint=os.path.join(checkpoint_dir, "vocoder.pth"),    # if needed
+#     #vocoder_config_path="path/to/vocoder_config.json",  # if needed
+# )
+
+# wav = synthesizer.tts(text, speaker_wav="PythonBE/GiangVoiceVi.wav", language="vi")
+
+# # Run TTS
+# # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# # Text to speech list of amplitude values as output
+# #wav = tts.tts(text, speaker_wav="PythonBE/GiangVoiceVi.wav", language="vi")
+# # Text to speech to a file
+# synthesizer.tts.tts_to_file(text, speaker_wav="PythonBE/GiangVoiceVi.wav", language="vi", file_path="PythonBE/EmDiHocSom.wav")
+
+
 import torch
 from TTS.api import TTS
-from TTS.tts.configs.xtts_config import XttsConfig
-
-# Allow the config class to be safely loaded
-torch.serialization.add_safe_globals([XttsConfig])
 
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -14,18 +57,12 @@ print(TTS().list_models())
 # Init TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-
-# Define the path to your .txt file
-text_file_path = "PythonBE/RogerPenroseEn.txt"
-text_file_path_vi = "PythonBE/EmDiHocSomVI.txt"
-
-# Read the contents of the file
-with open(text_file_path, 'r', encoding='utf-8') as file:
-    text = file.read()
-
 # Run TTS
 # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
 # Text to speech list of amplitude values as output
-wav = tts.tts(text, speaker_wav="PythonBE/GiangVoiceEn1.wav", language="en")
+text_file_path = "PythonBE/RogerPenroseEn.txt"
+with open(text_file_path, 'r', encoding='utf-8') as file:
+    text = file.read()
+wav = tts.tts(text, speaker_wav="PythonBE/GiangVoiceEn.wav", language="en")
 # Text to speech to a file
-tts.tts_to_file(text, speaker_wav="PythonBE/GiangVoiceEn1.wav", language="en", file_path="PythonBE/RogerPenrose_tts_output1.wav")
+tts.tts_to_file(text, speaker_wav="PythonBE/GiangVoiceEn.wav", language="en", file_path="PythonBE/RogerPenroseEn_output2.wav")
