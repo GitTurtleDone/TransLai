74b22c504d856f0003ab61a4644dba0ded7f8ec0
GitTurtleDone <giang2110@gmail.com>
2025-05-13 11:18:55 +1200

    Modularized the pipline and run from beginning to the end

--- PythonBE/combined_app.py
+++ PythonBE/combined_app.py
@@ -7,24 +7,24 @@ from transformers import MarianMTModel, MarianTokenizer
 
 # for running vixtts
 
-# import string
-# import unicodedata
-# from datetime import datetime
-from pprint import pprint
 
+from pprint import pprint
 import torch
 import torchaudio
 from tqdm import tqdm
 from underthesea import sent_tokenize
-from unidecode import unidecode
+# from unidecode import unidecode
 from IPython.display import clear_output
-from IPython.display import Audio
+# from IPython.display import Audio
 
-# the folder where processing files are stored
+# global variable
+ipt_lang = 'en'
+opt_lang = 'vi'
+process_folder = 'ProcessFiles' # the folder where processing files are stored
 
-process_folder = 'ProcessFiles'
+# clean process folder
 
-def clean_process_files_folder():
+def clean_process_folder():
 
     global process_folder
     
@@ -33,90 +33,82 @@ def clean_process_files_folder():
         if os.path.isfile(file_path):
             os.remove(file_path)
 
+# set up to record an audio
+ipt_audio_file_path = os.path.join(process_folder, "ipt.wav")
+audio_record_command = [
+    "ffmpeg",
+    "-f", "alsa",
+    "-i", "default",
+    ipt_audio_file_path
+]
+
 def record_audio():
-    global process_folder
-    audio_file_path = os.path.join(process_folder, "to_be_translated.wav")
-    command = [
-        "ffmpeg",
-        "-f", "alsa",
-        "-i", "default",
-        audio_file_path
-    ]
-    subprocess.run(command)
+    global audio_record_command
+    subprocess.run(audio_record_command)
+#--------------------------------------
 
-# record_audio(to_be_translated_file_path)
+# set up to transcribe the recorded audio
+whisper_ipt_audio_file_path = os.path.join("..", process_folder, "ipt.wav") # ".." is for signing out of the whisper.cpp folder
 
-def transcribe_input_audio(ipt_lang = 'en'):
+# Define the command to be run from the terminal
+whisper_command = [
+    "./build/bin/whisper-cli",
+    "-m", "models/ggml-tiny.bin",
+    "-f", whisper_ipt_audio_file_path,
+    "-otxt",
+    "-l", ipt_lang
+]
+
+# set working directory to where whisper.cpp is located
+whisper_working_dir = "whisper.cpp"  # ← change this to your actual path
+
+def transcribe_input_audio():
     
-    audio_file_path = os.path.join("..", process_folder, "to_be_translated.wav") # ".." is for signing out of the whisper.cpp folder
-
-    # Define the command to be run from the terminal
-    command = [
-        "./build/bin/whisper-cli",
-        "-m", "models/ggml-tiny.bin",
-        "-f", audio_file_path,
-        "-otxt",
-        "-l", ipt_lang
-    ]
-
-    # Set working directory to where whisper.cpp is located
-    working_dir = "whisper.cpp"  # ← change this to your actual path
+    global whisper_command
+    global whisper_working_dir
 
     # Run the command
-    result = subprocess.run(command, cwd=working_dir, capture_output=True, text=True)
+    result = subprocess.run(whisper_command, cwd=whisper_working_dir, capture_output=True, text=True)
 
     # Print output or error
     print("STDOUT:", result.stdout)
     print("STDERR:", result.stderr)
+#--------------------------------------
 
+# set up to translate the input text using MarianMT
+marianMT_model_name = 'Helsinki-NLP/opus-mt-' + ipt_lang + '-' + opt_lang
+tokenizer = MarianTokenizer.from_pretrained(marianMT_model_name)
+marianMT_model = MarianMTModel.from_pretrained(marianMT_model_name)
 
-def translate_text(ipt_lang='en', opt_lang='vi'):
+# define the path to the input.txt file
+ipt_txt_file_path = os.path.join('.', process_folder, 'ipt.wav.txt')#"./ProcessFiles/ipt.wav.txt"
+opt_txt_file_path = os.path.join(process_folder, "opt.txt")
 
-    global process_folder
-    model_name = 'Helsinki-NLP/opus-mt-' + ipt_lang + '-' + opt_lang
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
-    model = MarianMTModel.from_pretrained(model_name)
+def translate_text():
 
-    # Define the path to your .txt file
-    to_be_translated_file_path = os.path.join('.', process_folder, 'to_be_translated.wav.txt')#"./ProcessFiles/to_be_translated.wav.txt"
+    # set up to translate the input text using MarianMT
+    global marianMT_model_name
+    global tokenizer
+    global marianMT_model
+    global ipt_txt_file_path
+    global opt_txt_file_path
 
     # Read the contents of the file
-    with open(to_be_translated_file_path, 'r', encoding='utf-8') as file:
+    with open(ipt_txt_file_path, 'r', encoding='utf-8') as file:
         text = file.read()
 
  
     batch = tokenizer([text], return_tensors="pt")
-    gen = model.generate(**batch)
+    gen = marianMT_model.generate(**batch)
     translation = tokenizer.decode(gen[0], skip_special_tokens=True)
 
-    translated_file_path = os.path.join(process_folder, "translated.txt")
-
-    with open(translated_file_path, 'w', encoding='utf-8') as file:
+    with open(opt_txt_file_path, 'w', encoding='utf-8') as file:
         file.write(translation)
+#---------------------------------------
 
-
+# set up to tts the translated text
 # The inference code is adapted from https://github.com/coqui-ai/TTS/blob/dev/TTS/demos/xtts_ft_demo/xtts_demo.py
 # and https://github.com/thinhlpg/vixtts-demo/blob/main/viXTTS_Demo.ipynb
-
-
-translated_text_file_path = os.path.join(process_folder, 'translated.txt')
-with open(translated_text_file_path, 'r', encoding='utf-8') as file:
-    input_text = file.read()
-
-# @markdown Chọn ngôn ngữ:
-language = "Tiếng Việt" # @param ["Tiếng Việt", "Tiếng Anh","Tiếng Tây Ban Nha", "Tiếng Pháp","Tiếng Đức","Tiếng Ý", "Tiếng Bồ Đào Nha", "Tiếng Ba Lan", "Tiếng Thổ Nhĩ Kỳ", "Tiếng Nga", "Tiếng Hà Lan", "Tiếng Séc", "Tiếng Ả Rập", "Tiếng Trung (giản thể)", "Tiếng Nhật", "Tiếng Hungary", "Tiếng Hàn", "Tiếng Hindi"]
-# @markdown Văn bản để đọc. Độ dài tối thiểu mỗi câu nên từ 10 từ để đặt kết quả tốt nhất.
-translated_text_file_path = os.path.join(process_folder, "translated.txt")
-with open(translated_text_file_path, 'r', encoding='utf-8') as file:
-    text = file.read()
-# @markdown Chọn giọng mẫu:
-reference_audio = "PythonBE/GiangVoiceEn.wav" 
-# @markdown Tự động chuẩn hóa chữ (VD: 20/11 -> hai mươi tháng mười một)
-normalize_text = True # @param {type:"boolean"}
-# @markdown In chi tiết xử lý
-verbose = False # @param {type:"boolean"}
-# @markdown Lưu từng câu thành file riêng lẻ.
-output_chunks = False # @param {type:"boolean"}
 
 def cry_and_quit():
     clear_output()
@@ -128,7 +120,6 @@ try:
     from TTS.tts.models.xtts import Xtts
 except:
     cry_and_quit()
-
 
 def clear_gpu_cache():
     if torch.cuda.is_available():
@@ -153,15 +144,31 @@ def load_model(xtts_checkpoint, xtts_config, xtts_vocab):
     print("Model Loaded!")
     return XTTS_MODEL
 
+print("> Đang nạp mô hình...")
+try:
+    vixtts_model = None
+    if not vixtts_model:
+        vixtts_model = load_model(xtts_checkpoint="vixtts-demo/model/model.pth",
+                                xtts_config="vixtts-demo/config.json",
+                                xtts_vocab="vixtts-demo/vocab.json")
+except:
+    vixtts_model = load_model(xtts_checkpoint="vixtts-demo/model/model.pth",
+                                xtts_config="vixtts-demo/model/config.json",
+                                xtts_vocab="vixtts-demo/model/vocab.json")
+clear_output()
+print("> Đã nạp mô hình")
 
-# def get_file_name():
+# @markdown Chọn giọng mẫu:
+reference_audio = "PythonBE/sample_voice_" + opt_lang+ ".wav" 
+# @markdown Tự động chuẩn hóa chữ (VD: 20/11 -> hai mươi tháng mười một)
+normalize_text = True # @param {type:"boolean"}
+# @markdown In chi tiết xử lý
+verbose = False # @param {type:"boolean"}
+# @markdown Lưu từng câu thành file riêng lẻ.
+output_chunks = False # @param {type:"boolean"}
 
-#     global translated_text_file_path
-    
-#     filename = os.path.splitext(os.path.basename(translated_text_file_path))[0]
-#     current_datetime = datetime.now().strftime("%y%m%d%H%M%S")
-#     filename = f"{current_datetime}_{filename}"
-#     return filename
+
+
 
 
 def calculate_keep_len(text, lang):
@@ -181,7 +188,6 @@ def calculate_keep_len(text, lang):
     elif word_count < 10:
         return 13000 * word_count + 2000 * num_punct
     return -1
-
 
 def normalize_vietnamese_text(text):
     text = (
@@ -268,7 +274,7 @@ def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file,
         wav_chunk["wav"] = torch.tensor(wav_chunk["wav"][:keep_len])
 
         if output_chunks:
-            out_path = os.path.join(process_folder, "translated.wav")
+            out_path = os.path.join(process_folder, "opt.wav")
             torchaudio.save(out_path, wav_chunk["wav"].unsqueeze(0), 24000)
             if verbose:
                 print(f"Saved chunk to {out_path}")
@@ -276,7 +282,7 @@ def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file,
         wav_chunks.append(wav_chunk["wav"])
 
     out_wav = torch.cat(wav_chunks, dim=0).unsqueeze(0)
-    out_path = os.path.join(process_folder, "translated.wav")
+    out_path = os.path.join(process_folder, "opt.wav")
     torchaudio.save(out_path, out_wav, 24000)
 
     if verbose:
@@ -285,56 +291,52 @@ def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file,
     return out_path
 
 
-language_code_map = {
-    "Tiếng Việt": "vi",
-    "Tiếng Anh": "en",
-    "Tiếng Tây Ban Nha": "es",
-    "Tiếng Pháp": "fr",
-    "Tiếng Đức": "de",
-    "Tiếng Ý": "it",
-    "Tiếng Bồ Đào Nha": "pt",
-    "Tiếng Ba Lan": "pl",
-    "Tiếng Thổ Nhĩ Kỳ": "tr",
-    "Tiếng Nga": "ru",
-    "Tiếng Hà Lan": "nl",
-    "Tiếng Séc": "cs",
-    "Tiếng Ả Rập": "ar",
-    "Tiếng Trung (giản thể)": "zh-cn",
-    "Tiếng Nhật": "ja",
-    "Tiếng Hungary": "hu",
-    "Tiếng Hàn": "ko",
-    "Tiếng Hindi": "hi"
-}
+def text_to_speech():
+    global opt_txt_file_path 
+    global reference_audio
+    with open(opt_txt_file_path, 'r', encoding='utf-8') as file:
+        opt_text = file.read()
 
-print("> Đang nạp mô hình...")
-try:
-    vixtts_model = None
-    if not vixtts_model:
-        vixtts_model = load_model(xtts_checkpoint="vixtts-demo/model/model.pth",
-                                xtts_config="vixtts-demo/config.json",
-                                xtts_vocab="vixtts-demo/vocab.json")
-except:
-    vixtts_model = load_model(xtts_checkpoint="vixtts-demo/model/model.pth",
-                                xtts_config="vixtts-demo/model/config.json",
-                                xtts_vocab="vixtts-demo/model/vocab.json")
-clear_output()
-print("> Đã nạp mô hình")
+    if not os.path.exists(reference_audio):
+        print("⚠️⚠️⚠️Bạn chưa tải file âm thanh lên. Hãy chọn giọng khác, hoặc tải file của bạn lên ở bên dưới.⚠️⚠️⚠️")
+        audio_file=reference_audio
+    else:
+        audio_file = run_tts(vixtts_model,
+                lang=opt_lang,
+                tts_text=opt_text,
+                speaker_audio_file=reference_audio,
+                normalize_text=normalize_text,
+                verbose=verbose,
+                output_chunks=output_chunks,)
 
-if not os.path.exists(reference_audio):
-    print("⚠️⚠️⚠️Bạn chưa tải file âm thanh lên. Hãy chọn giọng khác, hoặc tải file của bạn lên ở bên dưới.⚠️⚠️⚠️")
-    audio_file="/content/model/vi_sample.wav"
-else:
-    audio_file = run_tts(vixtts_model,
-            lang=language_code_map[language],
-            tts_text=input_text,
-            speaker_audio_file=reference_audio,
-            normalize_text=normalize_text,
-            verbose=verbose,
-            output_chunks=output_chunks,)
+    subprocess.run(["ffplay", "-nodisp", "-autoexit", audio_file])
+#-----------------------------------
+clean_process_folder()
+record_audio()
+transcribe_input_audio()
+translate_text()
+text_to_speech()
+# # @markdown Chọn ngôn ngữ:
+# language = "Tiếng Việt" # @param ["Tiếng Việt", "Tiếng Anh","Tiếng Tây Ban Nha", "Tiếng Pháp","Tiếng Đức","Tiếng Ý", "Tiếng Bồ Đào Nha", "Tiếng Ba Lan", "Tiếng Thổ Nhĩ Kỳ", "Tiếng Nga", "Tiếng Hà Lan", "Tiếng Séc", "Tiếng Ả Rập", "Tiếng Trung (giản thể)", "Tiếng Nhật", "Tiếng Hungary", "Tiếng Hàn", "Tiếng Hindi"]
+# @markdown Văn bản để đọc. Độ dài tối thiểu mỗi câu nên từ 10 từ để đặt kết quả tốt nhất.
 
-
-subprocess.run(["ffplay", "-nodisp", "-autoexit", audio_file])
-
-
-
-
+# language_code_map = {
+#     "Tiếng Việt": "vi",
+#     "Tiếng Anh": "en",
+#     "Tiếng Tây Ban Nha": "es",
+#     "Tiếng Pháp": "fr",
+#     "Tiếng Đức": "de",
+#     "Tiếng Ý": "it",
+#     "Tiếng Bồ Đào Nha": "pt",
+#     "Tiếng Ba Lan": "pl",
+#     "Tiếng Thổ Nhĩ Kỳ": "tr",
+#     "Tiếng Nga": "ru",
+#     "Tiếng Hà Lan": "nl",
+#     "Tiếng Séc": "cs",
+#     "Tiếng Ả Rập": "ar",
+#     "Tiếng Trung (giản thể)": "zh-cn",
+#     "Tiếng Nhật": "ja",
+#     "Tiếng Hungary": "hu",
+#     "Tiếng Hàn": "ko",
+#     "Tiếng Hindi": "hi"
+# }
